fbe6589e2a8e4d8bdc38dd38b92c4a0388954d80
Corentin <maurerco@outlook.com>
2024-09-27 10:42:26 +0200

    créer et importer la classe App

---
+++ exercice1/class/App.js
@@ -0,0 +1,21 @@
+export default class App {
+  constructor() {
+    this.canvas;
+    this.ctx;
+  } // obligatoire pour créér une classe, déclarer les variables globales dans le constructeur
+
+  createCanvas(width, height) {
+    this.canvas = document.createElement("canvas");
+    this.canvas.width = width;
+    this.canvas.height = height;
+    document.body.appendChild(this.canvas);
+  }
+
+  circle(x, y, radius) {
+    // retirer le terme function à l'intérieur d'une classe
+    this.ctx = this.canvas.getContext("2d");
+    this.ctx.beginPath();
+    this.ctx.arc(x, y, radius, 0, 2 * Math.PI);
+    this.ctx.stroke();
+  }
+} // exporte la classe App pour pouvoir l'importer dans un autre fichier
---
+++ exercice1/main.js
@@ -0,0 +1,6 @@
+import App from "./class/App.js";
+
+let app = new App();
+
+app.createCanvas(400, 400);
+app.circle(200, 200, 100);
